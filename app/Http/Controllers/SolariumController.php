28b3b067ecfdbb90994281873f8733ae4a29eb5e
unknown <samsulhadikediri@gmail.com>
2021-04-08 16:06:21 +0700

    test solr

--- app/Http/Controllers/SolariumController.php
+++ app/Http/Controllers/SolariumController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Solarium\Client;
+use Solarium\Core\Client\Adapter\Curl;
+use Symfony\Component\EventDispatcher\EventDispatcher; 
 
 class SolariumController extends Controller
 {
@@ -17,7 +20,6 @@ class SolariumController extends Controller
     {
         // create a ping query
         $ping = $this->client->createPing();
-
         // execute the ping query
         try {
             $this->client->ping($ping);
@@ -25,5 +27,26 @@ class SolariumController extends Controller
         } catch (\Solarium\Exception $e) {
             return response()->json('ERROR', 500);
         }
+    }
+    // for tinker 
+    // $controller = app()->make('App\Http\Controllers\SolariumController');
+    // app()->call([$controller, 'add'], ['dokumen' => $dokumen]);
+    public function add($dokumen)
+    {
+        $adapter = new Curl();
+        $dispatcher = new EventDispatcher();
+
+        $client = new Client($adapter, $dispatcher, config('solarium'));
+
+        $update = $client->createUpdate();
+        $doc = $update->createDocument();
+        foreach( $dokumen->toArray() as $key => $value )
+        {
+            $doc->$key = $value;
+        }
+        $update->addDocuments(array($doc));
+        $update->addCommit();
+        $result = $client->update($update);
+        return $result->getStatus();
     }
 }
